c8b8b2a09473db2375d3b8db66b551d1aef8ae5d
默然 <moranzcw@gmail.com>
2017-07-24 00:26:24 +0800

    更新爬虫

---
+++ 001 zhihu/zhihu.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Required
+- requests (必须)
+- pillow (可选)
+Info
+- author : "xchaoinfo"
+- email  : "xchaoinfo@qq.com"
+- date   : "2016.2.4"
+Update
+- name   : "wangmengcn"
+- email  : "eclipse_sv@163.com"
+- date   : "2016.4.21"
+'''
+import requests
+try:
+    import cookielib
+except:
+    import http.cookiejar as cookielib
+import re
+import time
+import os.path
+try:
+    from PIL import Image
+except:
+    pass
+
+
+# 构造 Request headers
+agent = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36'
+headers = {
+    "Host": "www.zhihu.com",
+    "Referer": "https://www.zhihu.com/",
+    'User-Agent': agent
+}
+
+# 使用登录cookie信息
+session = requests.session()
+session.cookies = cookielib.LWPCookieJar(filename='cookies')
+try:
+    session.cookies.load(ignore_discard=True)
+except:
+    print("Cookie 未能加载")
+
+
+def get_xsrf():
+    '''_xsrf 是一个动态变化的参数'''
+    index_url = 'https://www.zhihu.com'
+    # 获取登录时需要用到的_xsrf
+    index_page = session.get(index_url, headers=headers)
+    html = index_page.text
+    pattern = r'name="_xsrf" value="(.*?)"'
+    # 这里的_xsrf 返回的是一个list
+    _xsrf = re.findall(pattern, html)
+    return _xsrf[0]
+
+
+# 获取验证码
+def get_captcha():
+    t = str(int(time.time() * 1000))
+    captcha_url = 'https://www.zhihu.com/captcha.gif?r=' + t + "&type=login"
+    r = session.get(captcha_url, headers=headers)
+    with open('captcha.jpg', 'wb') as f:
+        f.write(r.content)
+        f.close()
+    # 用pillow 的 Image 显示验证码
+    # 如果没有安装 pillow 到源代码所在的目录去找到验证码然后手动输入
+    try:
+        im = Image.open('captcha.jpg')
+        im.show()
+        im.close()
+    except:
+        print(u'请到 %s 目录找到captcha.jpg 手动输入' % os.path.abspath('captcha.jpg'))
+    captcha = input("please input the captcha\n>")
+    return captcha
+
+
+def isLogin():
+    # 通过查看用户个人信息来判断是否已经登录
+    url = "https://www.zhihu.com/settings/profile"
+    login_code = session.get(url, headers=headers, allow_redirects=False).status_code
+    if login_code == 200:
+        return True
+    else:
+        return False
+
+
+def login(secret, account):
+    _xsrf = get_xsrf()
+    headers["X-Xsrftoken"] = _xsrf
+    headers["X-Requested-With"] = "XMLHttpRequest"
+    # 通过输入的用户名判断是否是手机号
+    if re.match(r"^1\d{10}$", account):
+        print("手机号登录 \n")
+        post_url = 'https://www.zhihu.com/login/phone_num'
+        postdata = {
+            '_xsrf': _xsrf,
+            'password': secret,
+            'phone_num': account
+        }
+    else:
+        if "@" in account:
+            print("邮箱登录 \n")
+        else:
+            print("你的账号输入有问题，请重新登录")
+            return 0
+        post_url = 'https://www.zhihu.com/login/email'
+        postdata = {
+            '_xsrf': _xsrf,
+            'password': secret,
+            'email': account
+        }
+    # 不需要验证码直接登录成功
+    login_page = session.post(post_url, data=postdata, headers=headers)
+    login_code = login_page.json()
+    if login_code['r'] == 1:
+        # 不输入验证码登录失败
+        # 使用需要输入验证码的方式登录
+        postdata["captcha"] = get_captcha()
+        login_page = session.post(post_url, data=postdata, headers=headers)
+        login_code = login_page.json()
+        print(login_code['msg'])
+    # 保存 cookies 到文件，
+    # 下次可以使用 cookie 直接登录，不需要输入账号和密码
+    session.cookies.save()
+
+try:
+    input = raw_input
+except:
+    pass
+
+
+if __name__ == '__main__':
+    if isLogin():
+        print('您已经登录')
+    else:
+        account = input('请输入你的用户名\n>  ')
+        secret = input("请输入你的密码\n>  ")
+        login(secret, account)
--- spider.py
+++ spider.py
@@ -1,21 +1,84 @@
 import requests
 import re
+import json
+from threading import Thread
+from queue import Queue
+
+urlQueue = Queue()
+infoQueue = Queue()
+
+f = open(r'test.txt', 'r')  # 打开所保存的cookies内容文件
+cookies = {}  # 初始化cookies字典变量
+for line in f.read().split(';'):  # 按照字符：进行划分读取
+    # 其设置为1就会把字符串拆分成2份
+    name, value = line.strip().split('=', 1)
+    cookies[name] = value  # 为字典cookies添加内容
 
 headers = {
-    "Host":"www.zhihu.com",
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    "Host": "www.zhihu.com",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+    AppleWebKit/537.36 (KHTML, like Gecko) \
+    Chrome/58.0.3029.110 \
+    Safari/537.36"
 }
-r = requests.get("https://www.zhihu.com/people/moranzcw/following", headers = headers)
+user_id_regex = r'(?<=api/v4/people/).{32}'
+user_id_pattern = re.compile(user_id_regex)
+user_url_token_regex = r'(?<="url_token": ").{32}'
 
-print(r.status_code)
-# print(r.headers)
-print(r.encoding)
-# print(r.text)
 
-regex = r'(?<=href=")/people/.*?"'
-pattern = re.compile(regex)
-result = pattern.findall(r.text)
+# 9ffd5c016aabeafd1030e63fe410beb8
 
-for x in result:
-	print(x)
-	pass
+# {"avatar_url_template": "https://pic2.zhimg.com/c7d53b471237006a318de480af7fd44d_{size}.jpg",
+# "badge": [],
+# "type": "people",
+# "name": "\u8c46\u6d46\u6cb9\u6761",
+# "url": "http://www.zhihu.com/api/v4/people/9ffd5c016aabeafd1030e63fe410beb8",
+# "gender": 0,
+# "user_type": "people",
+# "is_advertiser": false,
+# "avatar_url": "https://pic2.zhimg.com/c7d53b471237006a318de480af7fd44d_is.jpg",
+# "is_org": false,
+# "headline": "\u5fae\u535a\uff1aCammmilla",
+# "url_token": "li-zi-qing-68-6",
+# "id": "9ffd5c016aabeafd1030e63fe410beb8"}
+
+
+def get_info(user_id):
+    response = requests.get("https://www.zhihu.com/api/v4/members/" + user_id, headers=headers, cookies=cookies)
+    # if response.status_code == 200:
+    print(response.status_code)
+    print(response.text)
+    if response.status_code == 200:
+        user_url_token_list = user_id_pattern.findall(response.text)
+    else:
+        user_url_token_list = []
+    # print (r.text)
+    return user_url_token_list
+
+
+class Worker(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+    def run(self):
+        # url = urlQueue.get()
+        # info = ()
+        # infoQueue.put(info)
+        pass
+
+
+class Master(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+    def run(self):
+        # url = ''
+        # urlQueue.put(url)
+        # infoQueue.get()
+        pass
+
+
+if __name__ == '__main__':
+    # print(headers)
+    resp = get_info("9ffd5c016aabeafd1030e63fe410beb8")
+    print(resp)
